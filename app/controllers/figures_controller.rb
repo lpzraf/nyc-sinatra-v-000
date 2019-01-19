a3195ed82f120908ef2b2ba388ccfb8ee03069e1
Rafael Lopez <lpz.raf@gmail.com>
2019-01-19 18:47:07 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class FiguresController < ApplicationController
   
   get '/figures/new' do
@@ -8,9 +10,25 @@ class FiguresController < ApplicationController
   
     post '/figures' do
     @figure = Figure.create(params[:figure])
-     if !params["title"]["name"].empty?
-      @figure.titles << Title.create(name: params["title"]["name"])
+     if !params["title"]["name"].empty? 
+      @figure.titles << Title.create(name: params["title"]["name"]) 
     end
    
+     if !params["landmark"]["name"].empty? 
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end
+    redirect "figures/#{@figure.id}"
    end
+   
+   get "/figures" do 
+     @figures = Figure.all
+    erb :'figures/index'
+   end
+   
+   get "/figures/:id" do 
+      @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+   end
+
+   
 end
